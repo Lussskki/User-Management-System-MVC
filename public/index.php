6020ad552d1120a3cf8969e061f29cc2ded5fd9b
Luka <lussskki@gmail.com>
2025-11-01 18:20:45 +0400

    add user controller for register

--- public/index.php
+++ public/index.php
@@ -1,16 +1,20 @@
 <?php
 require_once __DIR__ . '/../Router.php';
 require_once __DIR__ . '/../Database.php';
+require_once __DIR__ . '/../controllers/UserController.php';
 
 $router = new Router();
+$userController = new UserController();
 
 $router->get('/', function() {
-    return '<h1>Welcome to User Management System</h1>';
+    return '<h1>Welcome to User Management System</h1>
+            <a href="/user-managment-system/public/register">Go to Register</a>';
 });
 
 $router->get('/register', function() {
-    require '../views/register.php';
+    require __DIR__ . '/../views/register.php';
 });
+$router->post('/register', [$userController, 'registerSubmit']);
 
 $router->resolve();
 ?>
