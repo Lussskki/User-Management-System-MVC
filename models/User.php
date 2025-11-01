c2d466943d7f0eb29e4bc3401b6287ce2415c5d2
Luka <lussskki@gmail.com>
2025-11-01 22:05:43 +0400

    add function all

--- models/User.php
+++ models/User.php
@@ -15,5 +15,11 @@ class User {
         $stmt->execute([$name, $email, $hash]);
         return true;
     }
+     
+    public function all() {
+        $stmt = $this->pdo->query("SELECT id, name, email, created_at FROM users ORDER BY id DESC");
+        return $stmt->fetchAll();
+    }
+
 }
 ?>
