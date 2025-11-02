7dd274fb6ecf0ea4cf3fb1db62d8a086f6cffd3e
Luka <lukaguledani123@gmail.com>
2025-11-02 20:21:02 +0400

    add and resolve id pattern

--- Router.php
+++ Router.php
@@ -12,12 +12,22 @@ class Router {
 
     public function resolve() {
         $path = rtrim(str_replace('/User-Management-System-MVC/public', '', 
-        parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)), '/');
+            parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)), '/');
         if ($path === '') $path = '/';
-
         $method = $_SERVER['REQUEST_METHOD'];
-
         $callback = $this->routes[$method][$path] ?? null;
+        if (!$callback) {
+            foreach ($this->routes[$method] as $route => $cb) {
+                $pattern = preg_replace('/:[^\/]+/', '([^/]+)', $route);
+                $pattern = "#^" . $pattern . "$#";
+
+                if (preg_match($pattern, $path, $matches)) {
+                    array_shift($matches); 
+                    $callback = $cb;
+                    return call_user_func_array($callback, $matches);
+                }
+            }
+        }
         if (!$callback) {
             http_response_code(404);
             echo "404 - Page not found";
@@ -26,5 +36,6 @@ class Router {
 
         echo call_user_func($callback);
     }
+
 }
 ?>
